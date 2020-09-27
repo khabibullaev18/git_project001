ac47840334b22e12347fd53a3ac2c86d6df1c7b6
Muhammed Ali <khabibullaev.18@gmail.com>
2020-09-27 08:22:42 +0200

    last commit from local master branch

--- src/firs_package/FirstClass.java
+++ src/firs_package/FirstClass.java
@@ -5,5 +5,9 @@ public class FirstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on my Ali branch");
+		System.out.println("I want to have a conflict");
+		
+		System.out.println("I want to have a second conflict");
+		
 	}
 }
